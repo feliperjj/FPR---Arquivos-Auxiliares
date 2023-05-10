56904af75faa7792fa77061ca6c4654cf07df869
Felipe Bento <81867174+feliperjj@users.noreply.github.com>
2023-05-10 08:37:19 -0300

    Update Arquivos1_Escrever1aN.c

--- Arquivos - 28.04/Arquivos1_Escrever1aN.c
+++ Arquivos - 28.04/Arquivos1_Escrever1aN.c
@@ -1,7 +1,82 @@
 /*
     EXEMPLO 01:
-    Escrever em um arquivo numeros de 1 a n (aprimorei e coloquei em uma fun��o).
+    Escrever em um arquivo numeros de 1 a n (aprimorei e coloquei em uma função).
 */
+
+//importação de bibliotecas
+#include <stdio.h>
+#include <string.h>
+
+void divisores(char nomeArq[], char nomeArq2[]) {
+    FILE *arquivoOrig;
+    FILE *arquivoNovo;
+    int numero, i;
+    int processados[10];
+    int numerosJaAparece=0;
+    int encontrado;
+    
+
+    arquivoOrig = fopen(nomeArq, "r");
+	
+    if (arquivoOrig == NULL) {
+        printf("Erro ao abrir o arquivo %s.", nomeArq);
+        return;
+    }
+		arquivoNovo = fopen(nomeArq2, "w");
+    
+
+    if (arquivoNovo == NULL) {
+        printf("Erro ao criar o arquivo %s.", nomeArq2);
+        fclose(arquivoOrig);
+        return;
+    }
+    
+  	
+	
+
+    while (fscanf(arquivoOrig, "%d", &numero) != EOF) {
+        fprintf(arquivoNovo, "Divisores  %d: ", numero);
+        	for(i=0;i<numerosJaAparece;i++){
+    		
+    		if(processados[i] == numero){
+    			numerosJaAparece++;
+    				
+			}else if(numerosJaAparece<2){
+				
+				
+				 for (i = 1; i <= numero; i++) {
+            if ((numero % i == 0) && (numero != i-1)) {
+                fprintf(arquivoNovo, "%d ", i);
+            }
+        }
+
+        fprintf(arquivoNovo, "\n");
+    }				
+				
+				
+			}
+    		
+
+       
+
+    fclose(arquivoOrig);
+    fclose(arquivoNovo);
+}
+}
+
+int main() {
+    char nomeArqOriginal[20], nomeArqNovo[20];
+
+    strcpy(nomeArqOriginal, "teste.txt");
+    strcpy(nomeArqNovo, "novo.txt");
+
+    divisores(nomeArqOriginal, nomeArqNovo);
+
+    printf("O arquivo %s foi gerado com sucesso!\n", nomeArqNovo);
+
+    return 0;
+}
+
 
 #include <stdio.h>
 
