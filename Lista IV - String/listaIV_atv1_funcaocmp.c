c0381be8271d77c957252c3dbfc8f96d264a4ccc
Felipe Bento <81867174+feliperjj@users.noreply.github.com>
2023-05-10 08:39:40 -0300

    Atualizar o listaIV_atv1_funcaocmp.c

--- Lista IV - String/listaIV_atv1_funcaocmp.c
+++ Lista IV - String/listaIV_atv1_funcaocmp.c
@@ -1,4 +1,73 @@
-#include <stdio.h>
+##include <stdio.h>
+#include <string.h>
+
+void divisores(char nomeArq[], char nomeArq2[]) {
+    FILE *arquivoOrig;
+    FILE *arquivoNovo;
+    int numero, i;
+    int processados[10];
+    int numerosJaApareceu = 0;
+
+    arquivoOrig = fopen(nomeArq, "r");
+
+    if (arquivoOrig == NULL) {
+        printf("Erro ao abrir o arquivo %s.\n", nomeArq);
+        return;
+    }
+
+    arquivoNovo = fopen(nomeArq2, "w");
+
+    if (arquivoNovo == NULL) {
+        printf("Erro ao criar o arquivo %s.\n", nomeArq2);
+        fclose(arquivoOrig);
+        return;
+    }
+
+    while (fscanf(arquivoOrig, "%d", &numero) != EOF) {
+        int encontrado = 0;
+
+        for (i = 0; i < numerosJaApareceu; i++) {
+            if (processados[i] == numero) {
+                encontrado = 1;
+                break;
+            }
+        }
+
+        if (encontrado) {
+            continue;
+        }
+
+        fprintf(arquivoNovo, "Divisores de %d: ", numero);
+
+        for (i = 1; i <= numero; i++) {
+            if (numero % i == 0) {
+                fprintf(arquivoNovo, "%d ", i);
+            }
+        }
+
+        fprintf(arquivoNovo, "\n");
+
+        processados[numerosJaApareceu] = numero;
+        numerosJaApareceu++;
+    }
+
+    fclose(arquivoOrig);
+    fclose(arquivoNovo);
+}
+
+int main() {
+    char nomeArqOriginal[20], nomeArqNovo[20];
+
+    strcpy(nomeArqOriginal, "teste.txt");
+    strcpy(nomeArqNovo, "novo.txt");
+
+    divisores(nomeArqOriginal, nomeArqNovo);
+
+    printf("O arquivo %s foi gerado com sucesso!\n", nomeArqNovo);
+
+    return 0;
+}
+ <stdio.h>
 #define TAM 10
 
 int comparaString(char vet1[], char vet2[]){
@@ -41,7 +110,7 @@ int main(){
 		if (resultado<0){
 			printf("A primeira palavra em ordem alfabetica eh: %s", vet1);
 		} else {
-				printf("Ambos são iguais");
+				printf("Ambos sÃ£o iguais");
 		}
 	}
 }
